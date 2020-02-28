0bad48060df91bc6ec1992bad79a8590ba3088f2
Your Name <478855960@qq.com>
2020-02-28 16:06:32 +0800

    first commit

---
+++ WpfApp1/WpfApp1/Model/UserModel.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp1.Model
+{
+    public class UserModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        private int userId;
+        public int UserId
+        {
+            get
+            {
+                return userId;
+            }
+            set
+            {
+                userId = value;
+                NotifyPropertyChanged("UserId");
+            }
+        }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        private string userName;
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                NotifyPropertyChanged("UserName");
+            }
+        }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        private int userAge;
+        public int UserAge
+        {
+            get
+            {
+                return userAge;
+            }
+            set
+            {
+                userAge = value;
+                NotifyPropertyChanged("UserAge");
+            }
+        }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        private string userEmail;
+        public string UserEmail
+        {
+            get
+            {
+                return userEmail;
+            }
+            set
+            {
+                userEmail = value;
+                NotifyPropertyChanged("UserEmail");
+            }
+        }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        private string userSex;
+        public string UserSex
+        {
+            get
+            {
+                return userSex;
+            }
+            set
+            {
+                userSex = value;
+                NotifyPropertyChanged("UserSex");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/ViewModel/UserViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Input;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    public class UserViewModel
+    {
+        public DelegateCommand ShowCommand { get; set; }
+        public UserModel User { get; set; }
+        public UserViewModel()
+        {
+            User = new UserModel();
+            ShowCommand = new DelegateCommand();
+            ShowCommand.ExecuteCommand = new Action<object>(ShowUserData);
+        }
+        private void ShowUserData(object obj)
+        {
+            User.UserId = 1;
+            User.UserName = "xiaoyang";
+            User.UserAge = 22;
+            User.UserEmail = "478855960@qq.com";
+            User.UserSex = "靓仔";
+        }
+
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        public Action<object> ExecuteCommand = null;
+        public Func<object, bool> CanExecuteCommand = null;
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (CanExecuteCommand != null)
+            {
+                return this.CanExecuteCommand(parameter);
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.ExecuteCommand != null)
+            {
+                this.ExecuteCommand(parameter);
+            }
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
